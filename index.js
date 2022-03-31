d56c68c682cf241cc977102ec9d5b8b24bb118b8
Sal Castoro <20549092+scastoro@users.noreply.github.com>
2022-03-30 21:29:20 -0400

    Add passport.js LocalStrategy and jwt strategy
    Add and configure passport strategies to login user, return jwt and
    verify jwt on API call. Add route and logic to sign up users and
    encrypt passwords with bcrypt.

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const User = require('./models/user');
+const passportJWT = require('passport-jwt');
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
 
 const apiRouter = require('./routes/api');
-// const authRouter = require('./routes/auth')
+const passport = require('passport');
+const authRouter = require('./routes/auth');
 
 const app = express();
 
@@ -23,12 +28,32 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 //   next();
 // });
 
+passport.use(
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.SECRET,
+    },
+    async function (jwtPayload, cb) {
+      console.log(jwtPayload);
+      const user = await User.findById(jwtPayload._id).catch((err) => cb(err));
+
+      if (user) {
+        return cb(null, user);
+      } else {
+        return cb(null, false);
+      }
+    }
+  )
+);
+
 app.use(logger('dev'));
 app.use(express.json());
+app.use(passport.initialize());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api', apiRouter);
-// app.use('/auth', authRouter);
+app.use('/api', passport.authenticate('jwt', { session: false }), apiRouter);
+app.use('/auth', authRouter);
 
 app.use(function errorHandler(err, req, res, next) {
   console.dir(err);
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,74 @@
+require('dotenv').config();
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('../models/user');
+const { body, validationResult } = require('express-validator');
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    const user = await User.findOne({ username: username }).catch((err) => done(err));
+    if (!user) {
+      return done(null, false, { message: 'Incorrect username or password' });
+    }
+    const response = await bcrypt.compare(password, user.password).catch((err) => done(err));
+
+    if (response) {
+      console.log('Logged In!');
+      return done(null, user);
+    } else {
+      return done(null, false, { message: 'Incorrect Password' });
+    }
+    // return done(null, user);
+  })
+);
+
 const router = express.Router();
 
-router.post('/signup', function (req, res, next) {});
+router.post(
+  '/login',
+  passport.authenticate('local', { session: false, failureMessage: true }),
+  function (req, res, next) {
+    req.login(req.user, { session: false }, (err) => {
+      if (err) {
+        res.send(err);
+      }
+      console.log(req.user);
+      const token = jwt.sign(req.user.toJSON(), process.env.SECRET);
+      return res.json({ user: req.user, token });
+    });
+  }
+);
 
-router.post('/login', function (req, res, next) {});
+router.post(
+  '/signup',
+  body('username', 'Username must exist').trim().isLength({ min: 1 }).escape(),
+  body('password', 'Password must exist').trim().isLength({ min: 1 }).escape(),
+  async function (req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log(errors.array());
+      res.render('signup', { name: req.body.name, errors: errors.array(), title: 'Sign Up' });
+      return;
+    }
+
+    const hashedPassword = await bcrypt.hash(req.body.password, 10).catch(next);
+
+    console.log(hashedPassword);
+
+    const user = new User({
+      username: req.body.username,
+      password: hashedPassword,
+    });
+    const response = await user.save().catch(next);
+
+    if (response) {
+      console.log(response);
+      res.redirect('/');
+    }
+  }
+);
+
+module.exports = router;
